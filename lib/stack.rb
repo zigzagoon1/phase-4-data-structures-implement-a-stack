64a7b4ae67d16fe7024b9efeab2cbb7b55bd0f9b
Liz Burton <liz.burton147@gmail.com>
2021-11-01 13:09:25 -0400

    Add solution

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,41 @@
 # your code here
+class Stack
+  def initialize(limit = nil)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(item)
+    raise 'Stack Overflow' if @limit && @stack.length == @limit
+
+    @stack.push(item)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack[@stack.length - 1]
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.size.zero?
+  end
+
+  def full?
+    @limit && @stack.size == @limit
+  end
+
+  def search(target)
+    @stack.each_with_index do |item, idx|
+      return size - idx - 1 if item == target
+    end
+
+    -1
+  end
+end
