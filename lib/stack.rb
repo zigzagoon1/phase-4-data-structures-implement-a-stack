e44620bb11ea8e173f2fff93d0547b0cb9412db1
KellyMark <kmark602@gmail.com>
2023-10-28 22:19:47 -0400

    commit tree c712bebbefa155a908308f99aaba9d92d93df6fd

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,44 @@
 # your code here
+class Stack
+    attr_accessor :stack
+    
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if full?
+            raise 'Stack Overflow'
+        else
+        @stack.push(value)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        stack[stack.length - 1]
+    end
+
+    def size
+        @stack.length
+    end
+
+    def full?
+        return false unless @limit
+        @stack.size == @limit
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def search(target)
+        index = @stack.rindex(target)
+        index.nil? ? -1 : @stack.size - 1 - index
+    end
+
+end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -37,96 +37,96 @@ describe Stack do
   end
 
   # BONUS! Uncomment for an extra challenge.
-  # describe 'Bonus methods ⭐️' do
-  #   describe '.new with optional limit' do
-  #     it 'can be initialized with an optional limit attribute' do
-  #       stack = Stack.new(5)
-  #       expect(stack.instance_variable_get(:@limit)).to eq(5)
-  #     end
-  #   end
+  describe 'Bonus methods ⭐️' do
+    describe '.new with optional limit' do
+      it 'can be initialized with an optional limit attribute' do
+        stack = Stack.new(5)
+        expect(stack.instance_variable_get(:@limit)).to eq(5)
+      end
+    end
 
-  #   describe '.push with limit' do
-  #     let(:stack) do
-  #       stack = Stack.new(3)
-  #       stack.push(1)
-  #       stack.push(2)
-  #       stack
-  #     end
+    describe '.push with limit' do
+      let(:stack) do
+        stack = Stack.new(3)
+        stack.push(1)
+        stack.push(2)
+        stack
+      end
 
-  #     it 'adds the given element to the top of the stack if the stack is not full' do
-  #       stack.push(3)
-  #       expect(stack.instance_variable_get(:@stack)).to eq([1, 2, 3])
-  #     end
+      it 'adds the given element to the top of the stack if the stack is not full' do
+        stack.push(3)
+        expect(stack.instance_variable_get(:@stack)).to eq([1, 2, 3])
+      end
 
-  #     it 'throws an exception if the stack is full' do
-  #       stack.push(3)
-  #       expect { stack.push(4) }.to raise_error('Stack Overflow')
-  #     end
-  #   end
+      it 'throws an exception if the stack is full' do
+        stack.push(3)
+        expect { stack.push(4) }.to raise_error('Stack Overflow')
+      end
+    end
 
-  #   describe '#size' do
-  #     it 'returns the number of elements in the stack' do
-  #       stack = Stack.new
+    describe '#size' do
+      it 'returns the number of elements in the stack' do
+        stack = Stack.new
 
-  #       stack.push(1)
-  #       stack.push(2)
-  #       stack.push(3)
-  #       expect(stack.size).to eq(3)
-  #     end
-  #   end
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        expect(stack.size).to eq(3)
+      end
+    end
 
-  #   describe '#empty?' do
-  #     it 'returns true if the stack is empty' do
-  #       stack = Stack.new
-  #       expect(stack.empty?).to eq(true)
-  #     end
+    describe '#empty?' do
+      it 'returns true if the stack is empty' do
+        stack = Stack.new
+        expect(stack.empty?).to eq(true)
+      end
 
-  #     it 'returns false if the stack is not empty' do
-  #       stack = Stack.new
-  #       stack.push(10)
-  #       expect(stack.empty?).to eq(false)
-  #     end
-  #   end
+      it 'returns false if the stack is not empty' do
+        stack = Stack.new
+        stack.push(10)
+        expect(stack.empty?).to eq(false)
+      end
+    end
 
-  #   describe '#full?' do
-  #     let(:stack) do
-  #       stack = Stack.new(3)
+    describe '#full?' do
+      let(:stack) do
+        stack = Stack.new(3)
 
-  #       stack.push(1)
-  #       stack.push(2)
-  #       stack
-  #     end
+        stack.push(1)
+        stack.push(2)
+        stack
+      end
 
-  #     it 'returns true if the stack is full' do
-  #       stack.push(3)
-  #       expect(stack.full?).to eq(true)
-  #     end
-  #     it 'returns false if the stack is not full' do
-  #       expect(stack.full?).to eq(false)
-  #     end
-  #   end
+      it 'returns true if the stack is full' do
+        stack.push(3)
+        expect(stack.full?).to eq(true)
+      end
+      it 'returns false if the stack is not full' do
+        expect(stack.full?).to eq(false)
+      end
+    end
 
-  #   describe '#search' do
-  #     let(:stack) do
-  #       stack = Stack.new(5)
+    describe '#search' do
+      let(:stack) do
+        stack = Stack.new(5)
 
-  #       stack.push(1)
-  #       stack.push(2)
-  #       stack.push(3)
-  #       stack.push(4)
-  #       stack.push(5)
-  #       stack
-  #     end
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        stack.push(4)
+        stack.push(5)
+        stack
+      end
 
-  #     it 'returns the distance between the top of the stack and the target element' do
-  #       expect(stack.search(5)).to eq(0)
-  #       expect(stack.search(4)).to eq(1)
-  #       expect(stack.search(1)).to eq(4)
-  #     end
+      it 'returns the distance between the top of the stack and the target element' do
+        expect(stack.search(5)).to eq(0)
+        expect(stack.search(4)).to eq(1)
+        expect(stack.search(1)).to eq(4)
+      end
 
-  #     it 'returns -1 when the target is not in the stack' do
-  #       expect(stack.search(7)).to eq(-1)
-  #     end
-  #   end
-  # end
+      it 'returns -1 when the target is not in the stack' do
+        expect(stack.search(7)).to eq(-1)
+      end
+    end
+  end
 end
